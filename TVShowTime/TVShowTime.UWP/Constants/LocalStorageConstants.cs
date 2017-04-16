cd209e26d2c1d8fae0202d99269fe81bc0085ebf
David Bottiau <bottiau.david@laposte.net>
2017-04-16 12:35:48 +0200

    fix(notifications): launch notifications even if not scheduled (#3)

--- TVShowTime/TVShowTime.UWP/Constants/LocalStorageConstants.cs
+++ TVShowTime/TVShowTime.UWP/Constants/LocalStorageConstants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TVShowTime.UWP.BackgroundTasks;
+﻿using TVShowTime.UWP.BackgroundTasks;
 using TVShowTime.UWP.Views;
 
 namespace TVShowTime.UWP.Constants
@@ -14,11 +9,11 @@ namespace TVShowTime.UWP.Constants
         /// Key of the list of user connection profiles saved in local storage
         /// </summary>
         public const string UserConnectionProfiles = "userConnectionProfiles";
-
+        
         /// <summary>
-        /// Key of the list of episodes ids selected in <see cref="NewEpisodesBackgroundTask"/> saved in local storage
+        /// Key of the list of episodes ids already used (notification already shown) in <see cref="NewEpisodesV2BackgroundTask"/> saved in local storage
         /// </summary>
-        public const string NewEpisodesIdsSelected = "newEpisodesIdsSelected.txt";
+        public const string NewEpisodesIdsNotified = "newEpisodesIdsNotified.txt";
 
         /// <summary>
         /// Key of the item (EpisodeId) that will be used to navigate to <see cref="EpisodePage"/>
